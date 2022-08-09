6d61804461ee63dd3cd5a4a50fcbd20a3d0c4bba
Agacy junior <juniormartinssom@gmail.com>
2022-08-09 19:45:08 -0300

    Tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,35 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <PriceHighlight variant="income">
+                <td>R$ 12.000,00</td>
+              </PriceHighlight>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Alimentação</td>
+              <PriceHighlight variant="outcome">
+                <td>- R$ 1.000,00</td>
+              </PriceHighlight>
+              <td>Compra</td>
+              <td>14/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
-  )
+  );
 }
